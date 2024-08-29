d361f53b2d8750d50ea663d15e3f21e3d09dd03d
candebert <candelariabertoldi@gmail.com>
2024-08-29 19:45:49 +0200

    test: [green] get price return error [not implemented]

--- src/test/kotlin/com/gft/price_management/PriceManagementApplicationTests.kt
+++ src/test/kotlin/com/gft/price_management/PriceManagementApplicationTests.kt
@@ -1,13 +1,26 @@
 package com.gft.price_management
 
 import org.junit.jupiter.api.Test
+import com.gft.price_management.delivery.PriceManagementApiController
+import com.gft.price_management.delivery.model.PriceResponse
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.ResponseEntity
+import java.time.OffsetDateTime
+import kotlin.test.assertTrue
 
 @SpringBootTest
 class PriceManagementApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+	private val api: PriceManagementApiController = PriceManagementApiController()
 
+	@Test
+	fun priceManagementGetPriceGetTest() {
+		val testDate = OffsetDateTime.now()
+		val date: OffsetDateTime = testDate
+		val productId: Long = 1
+		val brandId = 1
+		val response: ResponseEntity<PriceResponse> = api.priceManagementGetPriceGet(date, productId, brandId)
+
+		assertTrue(response.statusCode.isError)
+	}
 }
